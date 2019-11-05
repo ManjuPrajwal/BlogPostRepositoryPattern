8398099e2e3d1bf7bcfa50cd4c076ee0cc7416b1
ManjuPrajwal <55299206+ManjuPrajwal@users.noreply.github.com>
2019-11-06 03:47:28 +0400

    Add files via upload

---
+++ Blog.sql
@@ -0,0 +1,56 @@
+CREATE TABLE [dbo].[BlogPostDetails](  
+    [BlogID] [int] IDENTITY(1,1) NOT NULL,  
+    [Title] [varchar](50) NULL,  
+    [Description] [varchar](50) NULL,  
+	[Rolid] int,
+	[Status] int,
+    [ImagePath] [varchar](500) NULL)
+
+
+
+
+create PROCEDURE InsertBlogPost
+
+@Title	varchar(max),
+@Description  [varchar](50) NULL,  
+@ImagePath varchar(max)
+
+AS 
+BEGIN
+
+
+
+
+INSERT INTO [dbo].BlogPostDetails
+           (Title,
+			Description,
+			ImagePath,			
+			Rolid,
+			Status)
+     VALUES
+           (@Title
+           ,@Description
+           ,@ImagePath
+           ,1
+           ,1)
+
+END
+
+
+
+
+
+create PROCEDURE GetBlogpostDetails
+
+
+
+AS 
+BEGIN
+
+
+
+
+select * from BlogPostDetails
+
+END
+
